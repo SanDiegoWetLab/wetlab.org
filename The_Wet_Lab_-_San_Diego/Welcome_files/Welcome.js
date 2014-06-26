049538d9ab668e0b82606b34674dbcdeb0cd3793
Alain Domissy <alaindomissy@gmail.com>
2014-06-25 23:31:48 -0700

    first commit

---
+++ The_Wet_Lab_-_San_Diego/Welcome_files/Welcome.js
@@ -0,0 +1,11 @@
+// Created by iWeb 3.0.4 local-build-20140625
+
+setTransparentGifURL('Media/transparent.gif');function applyEffects()
+{var registry=IWCreateEffectRegistry();registry.registerEffects({reflection_0:new IWReflection({opacity:0.50,offset:-4.93}),stroke_0:new IWEmptyStroke()});registry.applyEffects();}
+function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('Welcome_files/WelcomeMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');Widget.onload();fixupAllIEPNGBGs();fixAllIEPNGs('Media/transparent.gif');applyEffects()}
+function onPageUnload()
+{Widget.onunload();}
